4196fac9f4604f958503e56d49efa1e384f18ca7
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-02-09 20:40:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let strong = document.querySelectorAll("strong")
 function highlight() {
     //Write your code here
+	strong.forEach((ele)=>{
+		ele.style.color = "rgb(0, 128, 0)"
+	})
 
 
 }
@@ -7,6 +11,8 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
-
+	strong.forEach((ele)=>{
+		ele.style.color= "rgb(0, 0, 0)"
+	})
     
 }
